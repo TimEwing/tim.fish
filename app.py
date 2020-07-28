027bfe9e23a6047c83b56d40163d67dad523fed1
Tim <timjewing@gmail.com>
2020-07-28 17:34:56 -0600

    demo dropbox embedding

--- app.py
+++
@@ -1,7 +0,0 @@
-# app.py
-from flask import Flask, request, jsonify
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def index():
-    return "<h1>Hello World</h1>"
--- jstme/views.py
+++ jstme/views.py
@@ -1,7 +1,37 @@
-# app.py
+
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return "<h1>Hello World</h1> <p>I bought a domain name</p> <small>Did you know .fish was allowed? Me neither</small>"
+    return '''
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script type="text/javascript" src="https://www.dropbox.com/static/api/2/dropins.js" id="dropboxjs" data-app-key="aeaxg2ur27nt6gt"></script>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <div class="container-fluid">
+    <a
+      href="https://www.dropbox.com/sh/a880euoa5df3ra8/AACKKKKbq2qm8BYDKwJtDpSja?dl=0"
+      class="dropbox-embed"
+      data-height="100%"
+      data-width="100%"
+    ></a>
+    </div>
+  </body>
+</html>
+'''
