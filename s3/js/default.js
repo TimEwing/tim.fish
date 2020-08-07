8663512ce4f41699eabac9683b25ddbd8f9ccf9f
Tim <timjewing@gmail.com>
2020-08-06 20:36:27 -0600

    Fix welcome splash

--- s3/js/default.js
+++ s3/js/default.js
@@ -1,5 +1,8 @@
 
 $( document ).ready(function() {
+  // Defaults
+  var typingSpeed = 100; // ms per char
+
   // When internal navigation links are clicked, only update the content of the 
   // page while allowing the forward and back buttons to continue working
   $(".internal-nav").click(function(event){
@@ -26,7 +29,6 @@ $( document ).ready(function() {
       method: "GET",
       dataType: "json", // Parse response as json
       success: function(data) {
-        console.log(data);
         $("#title").prop("innerHTML", data.title);
         $("#content").prop("innerHTML", data.content);
       },
@@ -39,10 +41,94 @@ $( document ).ready(function() {
     $("#navbarNav").find("a[href=\""+url.pathname+"\"]").addClass("active");
   }
 
-  // When the page first loads, load whatever page was passed in as {{ target }}
-  target = $("#target").text();
-  console.log("target: "+target); 
-  history.pushState({}, "", target);
-  internalNav(target);
+  // Two different behaviors; base.html and welcome.html
+  if ($("#target").length) {
+    // base.html
+    // When the page first loads, load whatever page was passed in as {{ target }}
+    target = $("#target").text();
+    history.pushState({}, "", target);
+    internalNav(target);
+  }
+  else {
+    // welcome.html
+    // Render console with typing animation
+    var contentArray = $("#template-content").children().toArray();
+    renderConsole(null, contentArray, [], typingSpeed); 
+  }
+
+
+  /// Functions for the typewriter effect in the pseudo-console
+  function consoleNewline() {
+    // Add new line's element
+    $("#console").append("<span></span>");
+
+    var currentLine = $("#console").children().last();
+    // Add newline symbol
+    var contentTarget = $("#template-newline").clone().attr("id", "").appendTo(currentLine);
+    // Move cursor, or add one if it doesn't exist
+    if($("#cursor").length) {
+      $("#cursor").appendTo(currentLine);
+    }
+    else {
+      currentLine.append($("#template-cursor").clone().attr("id", "cursor"));
+    }
+    return contentTarget
+  }
+
+  function consoleNewElement(contentTarget, element) {
+    // If the new element has the newline class, add a new line
+    if(element.is(".newline")) {
+      var contentTarget = consoleNewline();
+    }
+    // Remove classes from element
+    element.removeClass("typewriter newline");
+    element.data("delay", null);
+    // Empty out text
+    element.text("");
+    // Add this element after old target
+    contentTarget.after(element);
+
+    return element;
+  }
+
+  function renderConsole(contentTarget, contentArray, textArray, delay) {
+    // If the content array is empty and there's no more target text, exit
+    if(contentArray.length == 0 && textArray.length == 0) {
+      // Make sure cursor is fixed before exiting
+      $("#cursor").addClass("blink");
+      return
+    }
+    // If textArray is empty, we need a new element
+    if(textArray.length == 0) {
+      // Pull element out of the content array
+      newElement = $(contentArray.shift());
+
+      // Pull out text and delay
+      delay = newElement.data("delay");
+      if(delay === undefined) {
+        delay = typingSpeed;
+      }
+
+      if(newElement.is(".typewriter")) {
+        textArray = newElement.text().split("");
+      }
+      else {
+        // This is "" sometimes; that's ok.
+        textArray = [newElement.text()];
+      }
+      // Make new element
+      contentTarget = consoleNewElement(contentTarget, newElement);
+    }
+    // If the delay is less than 750ms, disable the cursor blink
+    if(delay < 750) {
+      $("#cursor").removeClass("blink");
+    }
+    else {
+      $("#cursor").addClass("blink");
+    }
+    // Add an element of textArray to the contentTarget, then render more after a delay
+    contentTarget.append(textArray.shift());
+    setTimeout(renderConsole, delay, contentTarget, contentArray, textArray, delay); // last 3 args are sent to the function 
+  }
 
 });
